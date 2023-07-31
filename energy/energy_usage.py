143a19e0e020a6d581a74ca1433e8d54bd0052cc
Marcelo Torres <marcelo.torres.o@hotmail.com>
2023-07-31 00:22:44 -0300

    green energy usage calc

--- energy/energy_usage.py
+++ energy/energy_usage.py
@@ -1,16 +1,25 @@
 from bintrees import FastAVLTree
+import logging, sys
 
 # Bintrees doc: https://pypi.org/project/bintrees/
 
 
-class Work:
+if __name__ == '__main__':
+    log_level = logging.DEBUG
+else:
+    log_level = logging.INFO
+logging.basicConfig(stream=sys.stderr, level=log_level, format='[%(asctime)s][%(levelname)s] %(message)s')
+logger = logging.getLogger('energy_usage')
 
+
+class Work:
     def __init__(self, name, power):
         self.name = name
         self.power = power # in Watts, that is J/s
       
     def __str__(self):
         return f'{self.name} {self.power}W'
+
 
 class Instant:
     def __init__(self, timestamp, worksBeginning, worksEnding):
@@ -20,6 +29,7 @@ class Instant:
     
     def __str__(self):
         return str(self.timestamp)
+
 
 class EnergyUsage:
     
@@ -57,11 +67,12 @@ class EnergyUsage:
                     
                 energy = time * power # 1W * 1s = 1J
                 total_energy_in_joules += energy
-                #print(f'{time}s * Power={power}W = {energy}J')
+                logger.debug(f'{time}s * {power}W = {energy}J')
                 
                 # s, W and J
                 energy_trace.append((time, power, energy))
             else:
+                logger.debug(f'{time}s * 0W = 0J (No active works)')
                 energy_trace.append((time, 0, 0))
                 
             lastTime = instant.timestamp
@@ -78,35 +89,64 @@ class EnergyUsage:
         self.instants.clear()
 
 
-if __name__ == '__main__':
+def calc_green_energy_usage(energy_trace, next_green_interval):
 
-    # * * * * * * * * * * * * * * * * *  \/  * * * * * * * * * * * * * * * * * #
-    #                                                                          #
-    #                                  T E S T                                 #
-    #                                                                          #
-    # * * * * * * * * * * * * * * * * *  /\  * * * * * * * * * * * * * * * * * #
+    now = 0    
+    green_interval_time = 0
+    green_available_power = 0
 
-
-    # Works (Power)
-    # 
-    # E (  1W)                                    #####    
-    # D (  9W)                       #####             
-    # C (  7W)                      #####              
-    # B (100W)      ##########                         
-    # A ( 10W) #####                                     
-    #          ----+----+----+----+----+----+----+----+
-    #          1s  5s  10s  15s  20s  25s  30s  35s  40s
-
-    e = EnergyUsage()
+    total_energy = 0
+    total_brown_energy_used = 0
+    total_green_energy_used = 0
+    total_green_energy_not_used = 0
     
-    e.add_work('A', 0, 5, 10)
-    e.add_work('B', 5, 15, 100)
-    e.add_work('C', 21, 26, 7)
-    e.add_work('D', 22, 27, 9)
-    e.add_work('E', 35, 40, 1)
-    
-    total_energy_in_joules, energy_trace = e.calc()
-    
+    for time, power, energy in energy_trace:
+        
+        logger.debug('')
+        logger.debug(f'Energy Trace: {time}s, {power}W, {energy}J')
+
+        while time > 0:
+
+            if now >= green_interval_time:
+                green_interval_time, green_available_power = next_green_interval()
+
+            if now + time > green_interval_time:
+                logger.debug('Breaking the work duration to fit in the green interval')
+                t = green_interval_time - now
+            else:
+                t = time
+                
+            # Calculation
+            energy = t * power
+            green_energy = t * green_available_power
+            green_energy_used = energy if green_energy >= energy else green_energy
+            
+            total_energy += energy
+            total_brown_energy_used += energy - green_energy_used
+            total_green_energy_used += green_energy_used
+            total_green_energy_not_used += green_energy - green_energy_used
+            
+            logger.debug(f'({now}s t={t}s Energy={energy}J)')
+
+            now += t
+            time -= t
+            
+
+    return {
+        'total_energy': total_energy,
+        'total_brown_energy_used': total_brown_energy_used,
+        'total_green_energy_used': total_green_energy_used,
+        'total_green_energy_not_used': total_green_energy_not_used
+    }
+
+
+# * * * * * * * * * * * * * * * * *  \/  * * * * * * * * * * * * * * * * * #
+#                                                                          #
+#                                  T E S T                                 #
+#                                                                          #
+# * * * * * * * * * * * * * * * * *  /\  * * * * * * * * * * * * * * * * * #
+
+def __test_get_data():
     intervals_expected = [
     # duration (s), total power (W) and energy (J)
         (0, 0, 0),
@@ -119,26 +159,92 @@ if __name__ == '__main__':
         (8, 0, 0),
         (5, 1, 5) # E
     ]
-        
-    total_energy_in_joules, energy_trace = e.calc()
-    print(f'Energy usage intervals: {energy_trace}')
-    print(f'Total energy: {total_energy_in_joules}')
-  
-  
+
     expected_total_energy_in_joules = 0
-    for time, power, energy in intervals_expected:
+    for data in intervals_expected:
+        time, power, energy = data
         expected_total_energy_in_joules += energy
-        
-        t, p, e = energy_trace.pop(0)
+
+    return intervals_expected, expected_total_energy_in_joules
+
+
+def __test_calc():
+
+    # Works (Power)
+    # 
+    # E (  1W)                                    #####    
+    # D (  9W)                       #####             
+    # C (  7W)                      #####              
+    # B (100W)      ##########                         
+    # A ( 10W) #####                                     
+    #          ----+----+----+----+----+----+----+----+
+    #          1s  5s  10s  15s  20s  25s  30s  35s  40s
+
+
+    e = EnergyUsage()
+    
+    e.add_work('A', 0, 5, 10)
+    e.add_work('B', 5, 15, 100)
+    e.add_work('C', 21, 26, 7)
+    e.add_work('D', 22, 27, 9)
+    e.add_work('E', 35, 40, 1)
+    
+    total_energy_in_joules, energy_trace = e.calc()
+    logger.info(f'Energy usage intervals: {energy_trace}')
+    logger.info(f'Total energy: {total_energy_in_joules}')
+  
+    intervals_expected, expected_total_energy_in_joules = __test_get_data()
+    assert total_energy_in_joules == expected_total_energy_in_joules, f'Total energy should be {expected_total_energy_in_joules}'
+
+
+    for i, data in enumerate(intervals_expected):
+        time, power, energy = data  
+        t, p, e = energy_trace[i]
 
         assert t == time, f'Time {t} should be {time}'
         assert p == power, f'Power {p} should be {power}'
-        assert e == energy, f'Energy {e} should be {energy}'
-  
-    assert total_energy_in_joules == expected_total_energy_in_joules, f'Total energy should be {expected_total_energy_in_joules}'
-    
-    #e.add_work('A', 0, 30, 20)
-    #e.add_work('B', 25, 70, 20)
-    #e.add_work('C', 40, 80, 20)
-    #e.add_work('D', 60, 100, 20)
+        assert e == energy, f'Energy {e} should be {energy}'   
 
+
+def __test_calc_green_energy_usage():
+
+    # Power Usage: *
+    # Green Power Available: .
+    #
+    # 100W          **********
+    #  64W          '        '       ****
+    #  50W     *****' .............  '  '
+    #   9W        ....       '     ..'  '*    
+    #   7W     ...  '        '      *..................
+    #   5W          '        '      '    '        *****
+    #          ----+----+----+----+----+----+----+----+
+    #          1s  5s  10s  15s  20s  25s  30s  35s  40s
+
+    energy_trace, expected_total_energy_in_joules = __test_get_data()
+
+    green_power_intervals = [
+        #(0, 0), #??? TODO
+        (3, 7),
+        (7, 9),
+        (20, 50),
+        (22, 9),
+        (40, 7),
+        (0,0)
+    ]
+
+    i = iter(green_power_intervals)
+    def next_green_interval():
+        pv_interval_time, pv_available_power = next(i)
+        logger.debug(f'New Green Interval Read: {pv_interval_time}s Available Power: {pv_available_power}W')
+        return pv_interval_time, pv_available_power
+
+    logger.info('Calculating green energy usage...')
+    r = calc_green_energy_usage(energy_trace, next_green_interval)
+    logger.info(r)
+
+    assert r['total_energy'] == expected_total_energy_in_joules, f'Total energy should be {expected_total_energy_in_joules}'
+
+if __name__ == '__main__':
+
+    __test_calc()
+    __test_calc_green_energy_usage()
--- energy/main.py
+++ energy/main.py
@@ -1,14 +1,36 @@
 import json
+import logging, sys
 
+from datetime import datetime
 
 from energy_usage import EnergyUsage
+from energy_usage import calc_green_energy_usage
+
+
+logging.basicConfig(stream=sys.stderr, level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s')
+logger = logging.getLogger('main')
+
+
+def to_date_time(date_time_str):
+    date_format = '%Y-%m-%d %H:%M:%S'
+    return datetime.strptime(date_time_str, date_format)
 
 def microsecond_to_second(microsecond):
     return microsecond / 1000000
+    
+def joules_to_watts_hour(joules):
+    return joules/ 3600 # Wh = J/h = J/60*60 = J/3600s
+
+def print_energy(description, energy_in_joules):
+    energy_in_wh = joules_to_watts_hour(energy_in_joules)
+    logger.info('{}: {:.2f}Wh ({:.2f}J)'.format(description, energy_in_wh, energy_in_joules))
+    
+    
 
 def calc_energy_usage(events_file):
     e = EnergyUsage()
 
+    logger.info('Loading events...')
     f = open(events_file)
     data = json.load(f)
     
@@ -26,31 +48,65 @@ def calc_energy_usage(events_file):
             
             e.add_work('X', start, end, 35/4)
             
-    print(f'[i] {count} events loaded')
+    logger.info(f'{count} events loaded')
     f.close()
 
-    print('[i] Calculating energy usage...')
+    logger.info('Calculating energy usage intervals...')
     total_energy_in_joules, energy_trace = e.calc()
     e.clear()
     
     return total_energy_in_joules, energy_trace
+   
+   
+def calc_pv_energy_usage_2(energy_trace, pv_energy_file_name, pv_area, offset = 0):
+    
+    def read_pv_energy_line(pv_energy_file):
+        line = next(pv_energy_file, None)
+        if line is None:
+            raise Exception('End of pv energy file')
+            
+        row = line.strip().split(',')
+        return to_date_time(row[0]), int(row[1]), float(row[2])
+        
+    def next_pv_energy_interval():
+        d, pv_interval_time, solar_irradiance = read_pv_energy_line(pv_energy_file)
+        pv_available_power = pv_area * solar_irradiance
+        return pv_interval_time, pv_available_power
+        
+        
+    pv_energy_file = open(pv_energy_file_name)
 
-def calc_green_energy_usage(energy_trace, pv_energy_file):
-    pass
+    # ignore header
+    next(pv_energy_file)
+
+    if offset > 0:
+        pv_interval_time = 0
+        while pv_interval_time < offset:
+            d, pv_interval_time, solar_irradiance = read_pv_energy_line(pv_energy_file)
+
+    r = calc_green_energy_usage(energy_trace, next_pv_energy_interval)
+    
+    print_energy('Total Energy', r['total_energy'])
+    print_energy('Total Brown Energy Used', r['total_brown_energy_used'])
+    print_energy('Total Green Energy Used', r['total_green_energy_used'])
+    print_energy('Total Green Energy Not Used', r['total_green_energy_not_used'])
+
+    pv_energy_file.close()
     
 
 if __name__ == '__main__':
 
-    events_file = 'user_formatted.json'
-    pv_energy_file = ''
+    events_file = 'user_formatted_bigger.json'
+    #events_file = 'user_formatted.json'
+    #pv_energy_file = './../photovolta/data/photovolta_2016.csv_0.csv'
+    pv_energy_file = './../photovolta/photovolta_2016_part_1.csv'
+    pv_area = 1
+    offset = 29400
 
     total_energy_in_joules, energy_trace = calc_energy_usage(events_file)
     
     watts_hour = total_energy_in_joules / 3600
-    print('Total Energy:')
-    print("\t{:.2f}J".format(total_energy_in_joules))
-    print("\t{:.2f}Wh".format(watts_hour))
+    logger.info('Total Energy: {:.2f}Wh {:.2f}J'.format(watts_hour, total_energy_in_joules))
  
- 
-    # Closing file
-    
+    logger.info('Calculating green energy usage...')
+    calc_pv_energy_usage_2(energy_trace, pv_energy_file, pv_area, offset)
